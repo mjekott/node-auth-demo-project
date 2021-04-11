f799cbe751fe7fbcbf9478c9df6f4cc6e7e8daec
mjekott <ekottmfon@yahoo.com>
2021-04-11 12:59:13 +0100

    added joi for validation and added logic to check if user alreadye exist and save user to database if the user doesnt exist

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,20 @@
-import { Router } from 'express';
+const router = require('express').Router();
+const db = require('../db/connection');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
 
-const router = Router();
+const users = db.get('users');
+users.createIndex('username', { unique: true });
+
+const schema = Joi.object({
+  username: Joi.string()
+    .trim()
+    .regex(/(^[a-zA-Z0-9_]+$)/)
+    .min(3)
+    .max(30)
+    .required(),
+  password: Joi.string().min(8).required(),
+});
 
 router.get('/', (req, res) => {
   res.json({
@@ -8,10 +22,32 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/signup', (req, res) => {
-  res.json({
-    message: 'router is back',
-  });
+router.get('/signup', async (req, res, next) => {
+  try {
+    //validate req.body
+    const result = await schema.validateAsync(req.body);
+    //cehck is user exist in database
+    const exist = await users.findOne({ username: req.body.username });
+    if (exist) {
+      //throw an error if user exist
+      next(new Error('user aleady exist'));
+    }
+    //hash the user password using bcrypt
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    //create an object to hold the data we want to insert to database
+    const userObj = {
+      username: req.body.username,
+      password: hashedPassword,
+    };
+    //insert the user into the database
+    const user = await users.insert(userObj);
+    //delete password from object sent to client
+    delete user.password;
+    //return a response to the client
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
---
+++ db/connection.js
@@ -0,0 +1,4 @@
+const monk = require('monk');
+const db = monk('localhost/nodeAuth');
+
+module.exports = db;
