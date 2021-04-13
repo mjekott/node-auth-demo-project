d1223c5ca3aa94117006512332befa316df744e9
mjekott <ekottmfon@yahoo.com>
2021-04-13 15:49:03 +0100

    moved token generation into a function,so it can be reused

--- auth/index.js
+++ auth/index.js
@@ -9,6 +9,24 @@ const respondError422 = (res, next) => {
   next(new Error('Unable to login'));
 };
 
+const tokenSendResponse = async (user, res, next) => {
+  //we create a payload for our token.i.e the user id and username
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  //create a token with jwt.sign using the payload and secret and set expiring time of token
+  const token = await jwt.sign(payload, process.env.TOKEN_SECRET, {
+    expiresIn: '1d',
+  });
+  //we check if there is a problem on gerating token and return an error
+  if (!token) return respondError422(res, next);
+  //return token ato client
+  return res.json({
+    token,
+  });
+};
+
 const users = db.get('users');
 users.createIndex('username', { unique: true });
 
@@ -39,14 +57,14 @@ router.post('/signup', async (req, res, next) => {
       res.status(400);
       next(new Error('user aleady exist'));
     }
+    console.log('hello');
     //hash the user password using bcrypt
     user.password = await bcrypt.hash(user.password, 12);
     //save the user to database
     user = await users.insert(user);
     //delete password from object sent to client
-    delete user.password;
-    //return a response to the client
-    res.json(user);
+
+    tokenSendResponse(user, res, next);
   } catch (err) {
     res.status(422);
     next(err);
@@ -66,21 +84,8 @@ router.post('/login', async (req, res, next) => {
   const match = await bcrypt.compare(req.body.password, user.password);
   //we check if we have a match or return an error if no match
   if (!match) return respondError422(res, next);
-  //we create a payload for our token.i.e the user id and username
-  const payload = {
-    _id: user._id,
-    username: user.username,
-  };
-  //create a token with jwt.sign using the payload and secret and set expiring time of token
-  const token = await jwt.sign(payload, process.env.TOKEN_SECRET, {
-    expiresIn: '1d',
-  });
-  //we check if there is a problem on gerating token and return an error
-  if (!token) return respondError422(res, next);
-  //return token ato client
-  res.json({
-    token,
-  });
+
+  tokenSendResponse(user, res, next);
 });
 
 module.exports = router;
